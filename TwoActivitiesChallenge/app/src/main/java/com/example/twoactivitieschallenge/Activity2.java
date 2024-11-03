5e39c4319b2a19ba0c56be6657f6767d415be483
zaka <zakaria.harsoyo@gmail.com>
2024-11-03 17:49:41 +0700

    feat: initial commit for TwoActivities and TwoActivitiesChallenge

---
+++ TwoActivitiesChallenge/app/src/main/java/com/example/twoactivitieschallenge/Activity2.java
@@ -0,0 +1,25 @@
+package com.example.twoactivitieschallenge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        // Inisialisasi TextView
+        TextView textView = findViewById(R.id.textView);
+
+        // Mendapatkan Intent dan teks yang dikirim dari MainActivity
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("text_key");
+
+        // Menampilkan teks di TextView
+        textView.setText(text);
+    }
+}
